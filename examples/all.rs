d2a13103f7543cac28e04d5ba69c9c395771bbda
Tomasz Durda <tomekdur@wp.pl>
2022-09-18 11:25:50 +0200

    Updated example: added prepared statement to main example

--- examples/all.rs
+++ examples/all.rs
@@ -93,6 +93,22 @@ pub fn main() {
         }
     }
 
+    // Prepared statements
+    let statement = client.prepare_cypher(
+        "my_apache_graph",
+        "MATCH (n: Person) WHERE n.name = 'John' RETURN n",
+        false
+    ).unwrap();
+
+    match client.query(&statement, &[]) {
+        Ok(x) => {
+            let john_doe : Vertex<Person> = x[0].get(0);
+            assert_eq!(john_doe.properties().surname, "Doe");
+
+        },
+        Err(_) => assert!(false),
+    }
+
     // Constraints
     client
         .required_constraint("my_apache_graph", "Person", "myconstraint", "surname")
